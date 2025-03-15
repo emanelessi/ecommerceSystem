afefd6b0a5fa6e70457a48df7ce342b17f9ae4db
Eman El-Essi <hanieman86@gmail.com>
2025-03-15 21:58:54 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/clear-cache', function() {
-    \Artisan::call('config:clear');
-    \Artisan::call('cache:clear');
-    return 'Cache cleared!';
-});
