6e332e5e1a2137157794ccf445211062daa239de
Eman El-Essi <hanieman86@gmail.com>
2025-03-20 21:59:09 +0200

    add models and resource and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasRoles, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
+use Spatie\Permission\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
@@ -19,20 +19,76 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
+        Role::firstOrCreate(['name' => 'admin']);
+        Role::firstOrCreate(['name' => 'content manager']);
+        Role::firstOrCreate(['name' => 'order manager']);
+        Role::firstOrCreate(['name' => 'customer']);
 
+        $users = [
             [
                 'name' => 'admin',
                 'email' => 'admin@admin.com',
                 'email_verified_at' => now(),
                 'password' => Hash::make('123456'),
                 'remember_token' => Str::random(10),
-
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Content Manager',
+                'email' => 'content@store.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'remember_token' => Str::random(10),
+                'role' => 'content manager',
+            ],
+            [
+                'name' => 'Order Manager',
+                'email' => 'orders@store.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'remember_token' => Str::random(10),
+                'role' => 'order manager',
+            ],
+            [
+                'name' => 'Customer 1',
+                'email' => 'customer1@store.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'remember_token' => Str::random(10),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Customer 2',
+                'email' => 'customer2@store.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'remember_token' => Str::random(10),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Customer 3',
+                'email' => 'customer3@store.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'remember_token' => Str::random(10),
+                'role' => 'customer',
             ],
         ];
-        \DB::table('users')->insert($users);
 
+        foreach ($users as $userData) {
+            $user = User::create([
+                'name' => $userData['name'],
+                'email' => $userData['email'],
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'remember_token' => Str::random(10),
+            ]);
+
+            // تخصيص الدور للمستخدم
+            $role = Role::where('name', $userData['role'])->first();
+            if ($role) {
+                $user->assignRole($role);
+            }
+        }
     }
-
-
 }
